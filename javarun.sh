ad1ddd774bffa459265f940dd60f3ae2089bc5d4
Lucas Rosa <moeda.60@gmail.com>
2019-04-04 09:24:14 -0300

    Update javarun.sh

--- javarun.sh
+++ javarun.sh
@@ -2,13 +2,14 @@
 
 clear
 
-echo "javarun () {
+echo "Copying script to your bashrc"
+echo "javarun() {
 	read app
 	javac $app.java
 	java $app
-}" >> .bashrc
+}" >> ~/.bashrc
 
-source .bashrc
+echo "Updating bashrc"
+source ~/.bashrc
 
 echo "Instalation complete!"
-
